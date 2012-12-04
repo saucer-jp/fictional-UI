3082d3abb3bb5f0b9e2f2397cd17cd37add3c6a5
Tsukasa Obara <obara@saucer.jp>
2012-12-05 02:24:30 +0900

    log出力の実装

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 
 $(function(){
   HexSighter.run( 2 );
-  logger.run( 500, 20 );
+  logger.run( 80, 200 );
 });
 
 
@@ -52,13 +52,66 @@ var logger = {
   // --------------------
   // logs
   logs: [
-    'log'
+    '==> Upgrading postgresql',
+    '==> Downloading http://ftp.postgresql.org/pub/source/v9.1.2/postgresql-9.1.2.tar.bz2',
+    '######################################################################## 100.0%',
+    '==> ./configure --disable-debug --prefix=/usr/local/Cellar/postgresql/9.1.2 --enable-thread-safety --with-bonjour --with-gssapi --',
+    '==> make install-world',
+    '==> Caveats',
+    '# Build Notes',
+    'If builds of PostgreSQL 9 are failing and you have version 8.x installed,',
+    'you may need to remove the previous version first. See:',
+    '  https://github.com/mxcl/homebrew/issues/issue/2510',
+    'To build plpython against a specific Python, set PYTHON prior to brewing:',
+    '  PYTHON=/usr/local/bin/python  brew install postgresql',
+    'See:',
+    '  http://www.postgresql.org/docs/9.1/static/install-procedure.html',
+    '# Create/Upgrade a Database',
+    'If this is your first install, create a database with:',
+    '  initdb /usr/local/var/postgres',
+    'To migrate existing data from a previous major version (pre-9.1) of PostgreSQL, see:',
+    '  http://www.postgresql.org/docs/9.1/static/upgrading.html',
+    '# Start/Stop PostgreSQL',
+    'If this is your first install, automatically load on login with:',
+    '  mkdir -p ~/Library/LaunchAgents',
+    '  cp /usr/local/Cellar/postgresql/9.1.2/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/',
+    '  launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist',
+    'If this is an upgrade and you already have the homebrew.mxcl.postgresql.plist loaded:',
+    '  launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist',
+    '  cp /usr/local/Cellar/postgresql/9.1.2/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/',
+    '  launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist',
+    'Or start manually with:',
+    '  pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start',
+    'And stop with:',
+    '  pg_ctl -D /usr/local/var/postgres stop -s -m fast',
+    '# Loading Extensions',
+    'By default, Homebrew builds all available Contrib extensions.  To see a list of all',
+    'available extensions, from the psql command line, run:',
+    '  SELECT * FROM pg_available_extensions;',
+    'To load any of the extension names, navigate to the desired database and run:',
+    '  CREATE EXTENSION [extension name];',
+    'For instance, to load the tablefunc extension in the current database, run:',
+    '  CREATE EXTENSION tablefunc;',
+    'For more information on the CREATE EXTENSION command, see:',
+    '  http://www.postgresql.org/docs/9.1/static/sql-createextension.html',
+    'For more information on extensions, see:',
+    '  http://www.postgresql.org/docs/9.1/static/contrib.html',
+    '# Other',
+    'Some machines may require provisioning of shared memory:',
+    '  http://www.postgresql.org/docs/current/static/kernel-resources.html#SYSVIPC',
+    'To install postgresql ( and ossp-uuid ) in 32 bits mode; you may use --32-bit :',
+    '   brew install postgresql --32-bit',
+    'If you want to install the postgres gem, including ARCHFLAGS is recommended:',
+    '    env ARCHFLAGS="-arch x86_64" gem install pg',
+    'To install gems without sudo, see the Homebrew wiki.',
+    '==> Summary',
+    '/usr/local/Cellar/postgresql/9.1.2: 2742 files, 37M, built in 4.0 minutes'
   ],
 
   // getLog
-  getLog: function(){
-    var logStr = '';
-    logStr = this.logs[0];
+  getLog: function( logCount ){
+    var self = this;
+    var logStr = self.logs[ logCount ];
     return logStr;
   },
 
@@ -71,10 +124,25 @@ var logger = {
     var count = 0;
     var stopCount = stopCount;
 
+    var logs = self.logs;
+    var logLength = logs.length;
+    var logCount = 0;
+    var $logs = $('#log');
+
     var iterator = setInterval( logger, interval);
+    var logStr = '';
+
     function logger(){
-      self.printLog( self );
+      logStr = self.getLog( logCount );
+      self.printLog( $logs, logStr );
       count++;
+
+      if( logCount > logLength ){
+        logCount = 0;
+      } else {
+        logCount++;
+      }
+
       if( count > stopCount ){
         clearInterval( iterator );
       }
@@ -85,11 +153,15 @@ var logger = {
   // view
   // --------------------
   // printLog
-  printLog: function( self ){
-    var $logs = $('#log');
-    var logStr = self.getLog();
+  printLog: function( $logs, logStr ){
+
     var $log = $('<li/>');
     $log.text( logStr );
+
+    var listLength = $logs.find('li').length;
+    if( listLength > 20 ){
+      $logs.find('li:first').remove();
+    }
     $logs.append( $log );
   }
 };
