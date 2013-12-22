63c8cae0990bee8ca0439057ddb968e6ae3b0cd7
Tsukasa Obara <obara@saucer.jp>
2013-12-23 01:53:05 +0900

    SVG生成方法をcreateElementからcloneに変更

--- js/app.js
+++ js/app.js
@@ -21,14 +21,17 @@ var HexSighter = {
   // run
   run: function( sightSize ){
     var self = this;
-    var ID = 'hexSighters';
-    var $el = $('<ul/>', {id:ID});
+    var $el = $('.hexSighters');
+    var $svg = $('.hexSighter');
 
-    self.render( $el );
+    //self.render( $el );
 
     var indicators = [];
     for( var i = 0; i < sightSize; i++ ){
-      indicators[i] = new Indicator( $el, i ).run();
+      indicators[i] = new Indicator( $el, i, $svg ).run();
+      if( i === sightSize - 1 ){
+        $svg.remove();
+      }
     }
   },
 
@@ -36,9 +39,9 @@ var HexSighter = {
   // view
   // --------------------
   // render
-  render: function( $el ){
-    $('#container').prepend( $el );
-  }
+  //render: function( $el ){
+  //  $('#container').prepend( $el );
+  //}
 };
 
 
@@ -171,11 +174,12 @@ var logger = {
 // ********************
 // Indicator
 // ********************
-function Indicator( $parent, id ){
+function Indicator( $el, id, $svg ){
 
   var self = this;
-  var $parent = $parent;
+  var $parent = $el;
   var proto = Indicator.prototype;
+  var $svg = $svg;
 
   // --------------------
   // model
@@ -202,16 +206,13 @@ function Indicator( $parent, id ){
   // run
   this.run = function(){
     self.render();
-    self.event();
+    //self.event();
     self.setPrecision( 250 );
   };
 
   // event
   this.event = function(){
     var id = statuses.id;
-    var $el = $('.li' + id);
-    $el.on('click', function(){
-    });
   };
 
   // --------------------
@@ -221,23 +222,27 @@ function Indicator( $parent, id ){
   this.render = function(){
     var id = statuses.id;
 
-    var $indicator = $('<li/>').addClass( 'hexSighter' + id );
-    $indicator.text( id + ':' );
+    $el.append( $svg.clone().addClass( 'hexSighter' + id ) );
 
-    var hexSVG = self.createHexSVG(5);
-    $indicator.append( hexSVG );
 
-    var $precisions = $('<ul/>').addClass('precisions');
-    $precisions.append('<li/><li/>');
-    $indicator.append( $precisions );
+    //var $indicator = $('<li/>').addClass( 'hexSighter' + id );
+    //$indicator.text( id + ':' );
 
-    $parent.append( $indicator );
+    //var hexSVG = self.createHexSVG(5);
+    //$indicator.append( hexSVG );
+
+    //var $precisions = $('<ul/>').addClass('precisions');
+    //$precisions.append('<li/><li/>');
+    //$indicator.append( $precisions );
+
+    //$parent.append( $indicator );
   };
 
   // setPrecision
   this.setPrecision = function( interval ){
     var id = statuses.id;
     var $precisions = $('.hexSighter' + id ).find('.precisions li');
+    //var $precisions = $('.hexSighter').find('.precisions li');
     var iterator = setInterval( precision, interval );
 
     function precision(){
@@ -247,67 +252,67 @@ function Indicator( $parent, id ){
     }
   };
 
-  // createHex
-  proto.createHexSVG = function( size ){
-    var url = 'http://www.w3.org/2000/svg';
+  //// createHex
+  //proto.createHexSVG = function( size ){
+  //  var url = 'http://www.w3.org/2000/svg';
 
-    // hexs
-    var hexs = document.createElementNS( url, 'svg');
-    hexs.setAttribute('class','hexs');
+  //  // hexs
+  //  var hexs = document.createElementNS( url, 'svg');
+  //  hexs.setAttribute('class','hexs');
 
-    // filter
-    var defs = document.createElementNS( url, 'defs');
-    var filter = document.createElementNS( url, 'filter');
-    filter.setAttribute('id','filter');
+  //  // filter
+  //  var defs = document.createElementNS( url, 'defs');
+  //  var filter = document.createElementNS( url, 'filter');
+  //  filter.setAttribute('id','filter');
 
-    var feGB = document.createElementNS( url, 'feGaussianBlur');
-    feGB.setAttribute('in','SourceAlpha');
-    feGB.setAttribute('stdDeviation','2');
-    feGB.setAttribute('result','blur');
+  //  var feGB = document.createElementNS( url, 'feGaussianBlur');
+  //  feGB.setAttribute('in','SourceAlpha');
+  //  feGB.setAttribute('stdDeviation','2');
+  //  feGB.setAttribute('result','blur');
 
-    var feM = document.createElementNS( url, 'feMerge');
-    var feMN1 = document.createElementNS( url, 'feMergeNode');
-    var feMN2 = document.createElementNS( url, 'feMergeNode');
-    feMN1.setAttribute('in','blur');
-    feMN2.setAttribute('in','SourceGraphic');
-    feM.appendChild( feMN1 );
-    feM.appendChild( feMN2 );
+  //  var feM = document.createElementNS( url, 'feMerge');
+  //  var feMN1 = document.createElementNS( url, 'feMergeNode');
+  //  var feMN2 = document.createElementNS( url, 'feMergeNode');
+  //  feMN1.setAttribute('in','blur');
+  //  feMN2.setAttribute('in','SourceGraphic');
+  //  feM.appendChild( feMN1 );
+  //  feM.appendChild( feMN2 );
 
-    filter.appendChild( feGB );
-    filter.appendChild( feM );
-    defs.appendChild( filter );
-    hexs.appendChild( defs );
-    
-    // hex
-    var size = size || 1;
-    for( var i = 1; i <= size; i++ ){
-      hexs.appendChild( cloneHex() );
-    }
+  //  filter.appendChild( feGB );
+  //  filter.appendChild( feM );
+  //  defs.appendChild( filter );
+  //  hexs.appendChild( defs );
+  //  
+  //  // hex
+  //  var size = size || 1;
+  //  for( var i = 1; i <= size; i++ ){
+  //    hexs.appendChild( cloneHex() );
+  //  }
 
-    function cloneHex(){
-      var hex = document.createElementNS( url, 'polygon');
+  //  function cloneHex(){
+  //    var hex = document.createElementNS( url, 'polygon');
 
-      var translate = (function(){
-        var result = '';
-        var val = 10 * Math.random();
-        result = 'translate(' + val + ',' + val + ')';
-        return result;
-      })();
+  //    var translate = (function(){
+  //      var result = '';
+  //      var val = 10 * Math.random();
+  //      result = 'translate(' + val + ',' + val + ')';
+  //      return result;
+  //    })();
 
-      var scale = (function(){
-        var result = '';
-        var val = Math.random();
-        result = 'scale(' + val + ',' + val + ')';
-        return result;
-      })();
+  //    var scale = (function(){
+  //      var result = '';
+  //      var val = Math.random();
+  //      result = 'scale(' + val + ',' + val + ')';
+  //      return result;
+  //    })();
 
-      hex.setAttribute('class','hex');
-      hex.setAttribute('transform',translate + ',' + scale);
-      hex.setAttribute('filter','url(#filter)');
-      hex.setAttribute('points','20,70 0,35 20,0 59.9,0 79.8,35 59.9,70');
-      return hex;
-    }
+  //    hex.setAttribute('class','hex');
+  //    hex.setAttribute('transform',translate + ',' + scale);
+  //    hex.setAttribute('filter','url(#filter)');
+  //    hex.setAttribute('points','20,70 0,35 20,0 59.9,0 79.8,35 59.9,70');
+  //    return hex;
+  //  }
 
-    return hexs;
-  };
+  //  return hexs;
+  //};
 }
